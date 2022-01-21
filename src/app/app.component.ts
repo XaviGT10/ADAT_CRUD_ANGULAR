531542721ede4d531ed5055aa53f2e0123a9411e
Xavi Girón <xavier.giron@live.u-tad.com>
2022-01-21 11:44:00 +0100

    Root Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUD_Angular_MongoDB';
+  nombre = 'Xavi Girón Tomàs';
+  email = 'xavier.giron@live.u-tad.com';
+}
