1fe7081b338ef3d37b1f536f25a014c33b62ed44
Xavi Girón <xavier.giron@live.u-tad.com>
2022-01-21 13:41:03 +0100

    Añadido Angel Blanco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRUD_Angular_MongoDB';
-  nombre = 'Xavi Girón Tomàs';
-  email = 'xavier.giron@live.u-tad.com';
+  nombre1 = 'Angel Luis Blanco Rodríguez';
+  nombre2 = 'Xavi Girón Tomàs';
+  pagina2 = 'about.me/xavigiron'
+  email1 = 'angel.blanco@live.u-tad.com';
+  email2 = 'xavier.giron@live.u-tad.com';
+  rights = '© Todos los derechos reservados por U-Tad_va_a_acabar_con:nosotros SA.'
 }
