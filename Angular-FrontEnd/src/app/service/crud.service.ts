8e70b8eb085acca9961540065712544d46ead5ee
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2022-02-16 13:19:09 +0100

    Update services

--- Angular-FrontEnd/src/app/service/crud.service.ts
+++ Angular-FrontEnd/src/app/service/crud.service.ts
@@ -1,10 +1,80 @@
 import { Injectable } from '@angular/core';
 import { F1 } from './f1';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  // RUTA DE CONEXION A EXPRESS SERVER API
+  REST_API: String = 'http://localhost:8000/api';
+
+  // HTTP HEADER API
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+
+
+
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+
+  // INSERTAR COCHE
+  AddF1(data: F1): Observable<any> {
+    let API_URL = `${this.REST_API}/add-f1`;
+    return this.httpClient.post(API_URL,data)
+    .pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+  // COGER TODOS LOS OBJETOS
+
+  GetF1s(){
+    return this.httpClient.get(`${this.REST_API}`);
+  }
+
+  GetF1(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/detail-f1/${id}`;
+    return this.httpClient.get(API_URL, {headers: this.httpHeaders})
+    .pipe(map((res: any) => {
+      return res || {}
+    }),
+    catchError(this.handleError))
+  }
+
+
+  // BORRAR F1
+
+  deleteF1(id:any): Observable<any> {
+    let API_URL = `${this.REST_API}/deletef1/${id}`;
+    return this.httpClient.delete(API_URL, {headers: this.httpHeaders}).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+  // ERROR
+
+  handleError(error: HttpErrorResponse){
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      errorMessage = error.error.message
+    } else{
+      // Handle server error
+
+      errorMessage = `Error code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    console.log(errorMessage);
+    return throwError(errorMessage)
+  }
+
+
 }
