a715006b770525925fdcbfe7ae18a92064ded839
Ángel Blanco Rodríguez <73697059+AngelBlanco97@users.noreply.github.com>
2022-02-16 11:04:30 +0100

    Update app.module
    adding new imports modules

--- Angular-FrontEnd/src/app/app.module.ts
+++ Angular-FrontEnd/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { AddF1Component } from './components/add-f1/add-f1.component';
 import { DetailF1Component } from './components/detail-f1/detail-f1.component';
 import { ListF1Component } from './components/list-f1/list-f1.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -16,9 +20,13 @@ import { ListF1Component } from './components/list-f1/list-f1.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
